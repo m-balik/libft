f64ce83843a707ea09c9e95af7e132c258a975c8
m-balik <m.akbas2023@gtu.edu.tr>
2025-07-04 11:08:34 +0300

    Initial commit: Tüm libft dosyaları ve Makefile eklendi

---
+++ ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muakbas <muakbas@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 11:01:16 by muakbas           #+#    #+#             */
+/*   Updated: 2025/07/04 11:01:16 by muakbas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	str_size;
+
+	if (!s)
+		return (NULL);
+	str_size = ft_strlen(s);
+	if (str_size < start)
+	{
+		substr = (char *)malloc(sizeof(char) * 1);
+		if (!substr)
+			return (NULL);
+		substr[0] = '\0';
+		return (substr);
+	}
+	if (len > str_size - start)
+		len = str_size - start;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	ft_memcpy(substr, s + start, len);
+	substr[len] = '\0';
+	return (substr);
+}
